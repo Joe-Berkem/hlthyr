82f43a1eae6f78573b310960387a615fa0c3f9d7
Laura S <lausciampa@yahoo.com.ar>
2019-03-28 13:03:14 +0000

    header buttons: home, settings and log-out. Components, scss files, svg files (in assets folder)

---
+++ src/components/Buttons/HomeButton.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../../styles/css/main.css/main.css';
+import homeSVG from '../../styles/assets/homeButton.svg';
+
+
+const HomeButton = () => (
+
+	<img src={ homeSVG } className="homeButton" />
+
+);
+
+export default HomeButton;
---
+++ src/components/Buttons/SettingsButton.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../../styles/css/main.css/main.css';
+import settingGearsSVG from '../../styles/assets/settings-gears.svg';
+
+
+const SettingButton = () => (
+
+	<img src={ settingGearsSVG } className="settingGearsButton" />
+
+);
+
+export default SettingButton;
