e6546061514278357f61bb0a77f1c5870812d25d
Pete Latham <github@petelatham.com>
2019-03-29 09:27:36 +0000

    Stock changing in state when taken

--- src/components/DailyDoses/DailyDoses.js
+++ src/components/DailyDoses/DailyDoses.js
@@ -1,47 +1,58 @@
 import React from 'react';
 import DoseCard from '../DoseCard/';
 
+// Should this card be active? No if:
+//    the previous card is active
+const isDoseActive = (dose, i, array) => {
+  console.log('Array is ... ', array);
+  return true;
+};
+
 const DailyDoses = ({ doses }) => (
-
-    <section style={styles.container}>
-        <div style={styles.dateBanner}>
-            <h3 style={styles.dateBannerText}>Today</h3>
-        </div>
-        {doses.map(dose => 
-        <DoseCard
-            time={dose.time}
-            medName={dose.medName} 
-            medColour={dose.medColour} 
-            dose={dose.dose}
-            unit={dose.unit}
-        /> ) 
-        }
-
-    </section>
-)
+  <section style={styles.container}>
+    <div style={styles.dateBanner}>
+      <h3 style={styles.dateBannerText}>Today</h3>
+    </div>
+    {doses.map((dose, i, array) => (
+      <DoseCard
+        key={i}
+        id={dose.id}
+        time={dose.time}
+        medName={dose.medName}
+        medColour={dose.medColour}
+        dose={dose.dose}
+        unit={dose.unit}
+        isActive={() => isDoseActive(dose, i, array)}
+        isVisible={() => dose.isVisible}
+        taken={dose.taken}
+        missed={dose.missed}
+      />
+    ))}
+  </section>
+);
 
 export default DailyDoses;
 
 const styles = {
-    container: {
-        marginTop: '1em',
-        width: '100%',
-        borderRadius: '8px',
-        display: 'flex',
-        flexDirection: 'column',
-        justifyContent: 'space-evenly',
-        alignItems: 'center',
-    },
-    dateBanner: {
-        width: '85%',
-        backgroundColor: '#5271ff',
-        minHeight: '3em',
-        borderRadius: '8px',
-        textAlign: 'left',
-        padding: 'auto, 0',
-    }, 
-    dateBannerText: {
-        paddingLeft: '1em', 
-        color: 'white',
-    }
-}
+  container: {
+    marginTop: '1em',
+    width: '100%',
+    borderRadius: '8px',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-evenly',
+    alignItems: 'center',
+  },
+  dateBanner: {
+    width: '85%',
+    backgroundColor: '#5271ff',
+    minHeight: '3em',
+    borderRadius: '8px',
+    textAlign: 'left',
+    padding: 'auto, 0',
+  },
+  dateBannerText: {
+    paddingLeft: '1em',
+    color: 'white',
+  },
+};
--- src/data/reducers.js
+++ src/data/reducers.js
@@ -1,9 +1,28 @@
-
-
 const reducers = (state, action) => {
-        switch (action.type) {
-            default: return state;
-        }
+  switch (action.type) {
+    case 'checkBoxDoom':
+      return updateDoses(state, action);
+
+    default:
+      return state;
+  }
+};
+
+const updateDoses = (state, action) => {
+  const stateCopy = state;
+  const med_id = stateCopy.doses[action.id].med_id;
+  const doseSize = +stateCopy.doses[action.id].dose;
+
+  if (action.stuff === 'taken' && action.isChecked) {
+    stateCopy.doses[action.id][action.stuff] = action.isChecked;
+    stateCopy.meds[med_id].stock -= doseSize;
+    if (stateCopy.meds[med_id].stock < 0) stateCopy.meds[med_id].stock = 0;
+  } else if (action.stuff === 'taken' && !action.isChecked) {
+    stateCopy.doses[action.id][action.stuff] = action.isChecked;
+    stateCopy.meds[med_id].stock += doseSize;
+  }
+
+  return { ...stateCopy };
 };
 
 export default reducers;
